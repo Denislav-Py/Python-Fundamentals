faf47fcf227defdea17b751c54a84ea27df9e654
Denislav Iliev <dbiliev@gmail.com>
2025-05-24 21:35:53 +0200

    Create sum_of_chars.py

---
+++ Data Types & Variables/sum_of_chars.py
@@ -0,0 +1,14 @@
+# Write a program, which sums the ASCII codes of N characters and prints the sum on the console.
+# On the first line, you will receive N – the number of lines. On the following N lines – you will receive a letter per line.
+# Print the total sum in the following format: "The sum equals: {total_sum}".
+# Note: n will be in the interval [1…20].
+
+number_of_lines = int(input())
+
+total = 0
+
+for _ in range(number_of_lines):
+    character = input()
+    total += ord(character)
+
+print(f"The sum equals: {total}")
