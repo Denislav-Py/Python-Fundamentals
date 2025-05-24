a01a9c4e0cf6ae59952538a9843bccf0770fbaae
Denislav Iliev <dbiliev@gmail.com>
2025-05-24 21:20:18 +0200

    Create chars_to_string.py

---
+++ Data Types & Variables/chars_to_string.py
@@ -0,0 +1,8 @@
+# Write a function that receives 3 characters.
+# Concatenate all the characters into one string and print it on the console.
+
+first_char = input()
+second_char = input()
+third_char = input()
+
+print(f"{first_char}{second_char}{third_char}")
